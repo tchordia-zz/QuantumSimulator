b8f543928bc7a8d451775e7ee879744d98ae62ca
tchordia <tchordia@gmail.com>
2015-12-29 16:00:54 -0800

    Initial Commit

---
+++ Turing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Turing
+//
+//  Created by Tanmay Chordia on 12/29/15.
+//  Copyright © 2015 Dots. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
